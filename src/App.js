f67d3e709796e1e55cbce0663e48d6db939d3be1
unknown <minthantkyaw47@gmail.com>
2021-10-15 11:49:35 +0630

    finished last page

--- src/App.js
+++ src/App.js
@@ -4,21 +4,55 @@ import Home from "./component/Home";
 import Counter from "./component/Counter/counter";
 import NotFoundPage from "./component/NotFoundPage/NotFoundPage";
 import Landing from "./component/Landing/Landing";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import Photo from "./component/Photo/Photo";
 import { useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import ConfettiModal from "./component/Confetti";
 import Letter from "./component/Letter/Letter";
 import ModalBox from "./component/ModalBox/ModalBox";
+import Last from "./component/Last/Last";
+import birthdaySong from "./Assets/birthday song.mp3";
+import crowd from "./Assets/crowd saying hb.mp3";
 
 function App() {
   const [person, setPerson] = useState([]);
   const location = useLocation();
   const [happyTime, setHappyTime] = useState(false);
   const [open, setOpen] = useState(false);
+  const bdRef = useRef();
+  const crowdRef = useRef();
+  const crowdRef2 = useRef();
+
+  useEffect(() => {
+    if (happyTime) {
+      crowdRef.current.play();
+
+      setTimeout(function () {
+        crowdRef2.current.play();
+      }, 2000);
+
+      setTimeout(function () {
+        bdRef.current.play();
+      }, 2000);
+    }
+  }, [happyTime]);
+
   return (
     <>
+      {happyTime && (
+        <>
+          <audio ref={bdRef}>
+            <source src={birthdaySong} type="audio/mp3" />
+          </audio>
+          <audio ref={crowdRef} controls>
+            <source src={crowd} type="audio/mp3" />
+          </audio>
+          <audio ref={crowdRef2} controls>
+            <source src={crowd} type="audio/mp3" />
+          </audio>
+        </>
+      )}
       <ModalBox open={open} setOpen={setOpen} />
       <AnimatePresence exitBeforeEnter initial={false}>
         {happyTime && <ConfettiModal />}
@@ -42,6 +76,10 @@ function App() {
 
           <Route path="/letter">
             <Letter person={person} setOpen={setOpen} />
+          </Route>
+
+          <Route path="/last">
+            <Last person={person} />
           </Route>
 
           <Route path="/*">
---
+++ src/component/Last/Last.js
@@ -0,0 +1,57 @@
+import "./Last.css";
+import { motion } from "framer-motion";
+import {
+  containerVariants,
+  imageVariants,
+  buttonHoverVariants,
+} from "../../Animation";
+import { useHistory } from "react-router-dom";
+
+const Last = ({ person }) => {
+  const history = useHistory();
+  return (
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      className="last"
+    >
+      {person.length !== 0 && (
+        <>
+          <div className="last-img">
+            <motion.img
+              variants={imageVariants}
+              src={person[0].fields.lastPhoto.fields.file.url}
+              alt=""
+            />
+          </div>
+          <div className="last-content">
+            <motion.h1 variants={containerVariants} id="happyWord">
+              Happy
+            </motion.h1>
+            <motion.h1 variants={containerVariants} id="birthdayWord">
+              Birthday
+            </motion.h1>
+            <motion.p
+              variants={containerVariants}
+              className="birthdayLast text"
+            >
+              {person[0].fields.lastPhoto.fields.title}
+            </motion.p>
+            <motion.button
+              variants={buttonHoverVariants}
+              whileHover="hover"
+              className="button-primary"
+              onClick={() => history.goBack()}
+            >
+              Go Back
+            </motion.button>
+          </div>
+        </>
+      )}
+    </motion.div>
+  );
+};
+
+export default Last;
